13584a4b1ae849cbd41203992abf9ae0ce3ef3c6
ST10509503 <ST10509503@myemeris.edu.za>
2026-03-06 19:30:51 +0200

    I added the order class to handle the order related methods

--- CampusQuickServe_ICE/src/campusquickserve_ice/CampusQuickServe_ICE.java
+++ CampusQuickServe_ICE/src/campusquickserve_ice/CampusQuickServe_ICE.java
@@ -1,10 +1,13 @@
 package campusquickserve_ice;
+//JOptionnPane was imported to show dialog boxes
+import javax.swing.JOptionPane;
 
-
+//order class is created to show the methods related to the order
+class Order {
 
 
 public class CampusQuickServe_ICE {
 
 public static void main(String[] args) {
     }
-}
+}}
